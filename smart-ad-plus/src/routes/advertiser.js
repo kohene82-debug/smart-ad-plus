edbfb3ad9eebd080591f30cbe372f66cd25a7f2f
kohene82-debug <kohene82@gmail.com>
2026-04-05 23:03:35 +0800

    Fix advertiser routes - use authenticateUser for ads

--- smart-ad-plus/src/routes/advertiser.js
+++ smart-ad-plus/src/routes/advertiser.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router  = express.Router();
 const ctrl    = require('../controllers/advertiserController');
-consconst { authenticateAdvertiser, authenticateUser } = require('../middlewares/auth');t { authenticateAdvertiser } = require('../middlewares/auth');
+const { authenticateAdvertiser, authenticateUser } = require('../middlewares/auth');
 const { validate, schemas } = require('../middlewares/validate');
 
 router.post('/register',      validate(schemas.advertiserRegister), ctrl.register);
 router.post('/login',         validate(schemas.advertiserLogin),    ctrl.login);
-router.post('/createAd', authenticateUser, validate(schemas.createAd), ctrl.createAd);
-router.get('/ads',       authenticateUser, ctrl.getAds);router.post('/fundAccount',   authenticateAdvertiser, validate(schemas.fundAccount),  ctrl.fundAccount);
+router.post('/createAd',      authenticateUser, validate(schemas.createAd),    ctrl.createAd);
+router.get('/ads',            authenticateUser, ctrl.getAds);
+router.post('/fundAccount',   authenticateAdvertiser, validate(schemas.fundAccount),  ctrl.fundAccount);
 router.post('/payment/init',  authenticateAdvertiser, ctrl.verifyPayment);
 
 module.exports = router;
